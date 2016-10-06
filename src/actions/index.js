f3e4ee8b9929441f621c67b5c686d84bd4f8faea
Amanda Milburn <milburn.amanda@gmail.com>
2016-10-06 12:09:21 -0400

    adding post on form submit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=jfsszz08437xxji9';
@@ -11,6 +12,17 @@ export function fetchPosts() {
 	// return an action with request on the payload.
 	return {
 		type: FETCH_POSTS,
+		payload: request
+	};
+}
+
+// createPost is called with keys (title, content, categories) as arg
+export function createPost(props) {
+	// when we post to URL, pass along props as second arg
+	const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props);
+
+	return {
+		type: CREATE_POST,
 		payload: request
 	};
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
 import { reduxForm } from 'redux-form';
+// Import Action Creator
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
 	render() {
 
 		// Accessing handleSubmit props from reduxForm
 		const handleSubmit = this.props.handleSubmit;
+		// Accessing helpers to manage inputs from reduxForm
+		const title = this.props.fields.title;
+		const categories = this.props.fields.categories;
+		const content = this.props.fields.content;
 
 		return (
-			<form onSubmit={handleSubmit}>
+			// on submit, call action creator with form contents
+			<form onSubmit={handleSubmit(this.props.createPost)}>
 				<h3>Create A New Post</h3>
 				<div className="form-group">
 					<label>Title</label>
-					<input type="text" className="form-control" />
+					{/* Pass configuration object into input tag as a property */}
+					<input type="text" className="form-control" {...title} />
 				</div>
 				<div className="form-group">
 					<label>Categories</label>
-					<input type="text" className="form-control" />
+					<input type="text" className="form-control" {...categories} />
 				</div>				
 				<div className="form-group">
 					<label>Content</label>
-					<input type="textarea" className="form-control" />
+					<input type="textarea" className="form-control" {...content} />
 				</div>
 				<button type="submit" className="btn btn-primary">Submit</button>
 			</form>
@@ -28,9 +36,12 @@ class PostsNew extends Component {
 	}
 }
 
+/* reduxForm has the same behaviour as connect.
+connect: first arg is mapStateToProps, second is mapDispatchToProps
+reduxForm: first arg is form config, second is mapStateToProps, third is mapDispatchToProps */
 export default reduxForm({
 	// Form name needs to be unique
 	form: 'PostsNewForm',
 	// Unique inputs that the form will contain
 	fields: ['title', 'categories', 'content']
-})(PostsNew);
+}, null, { createPost })(PostsNew);
